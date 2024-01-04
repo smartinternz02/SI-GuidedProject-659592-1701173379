c6ef7c1b0238176a04f84fac5d883659da28aa63
myp-cloud <152283500+myp-cloud@users.noreply.github.com>
2024-01-05 00:14:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request,session
-
+import ibm_db
 app = Flask(__name__)
 app.secret_key ='a'
+
 def showall():
     sql= "SELECT * from USER"
     stmt = ibm_db.exec_immediate(conn, sql)
@@ -36,14 +37,14 @@ def insertdb(conn,name,email,contact,address,role,branch,password):
     print ("Number of affected rows: ", ibm_db.num_rows(stmt))
     
     
-import ibm_db
+#import ibm_db
 conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=mwv02817;PWD=BGu2G7vYyF7JGCrq",'','')
 print(conn)
 print("connection successful...")
 
 @app.route('/')
 def index():
-    return render_template('adminRegister.html')
+    return render_template('index.html')
 
 @app.route('/register', methods=['POST','GET'])
 def register():
@@ -58,31 +59,76 @@ def register():
         else:
             role = "Student"
         branch = request.form['branch']
-        password = request.form['pwd']
+        password = request.form['password']
         
         #inp=[name,email,contact,address,role,branch,password]
         insertdb(conn,name,email,contact,address,role,branch,password)
         return render_template('login.html')
         
 
-@app.route('/login', methods=['POST','GET'])
-def login():
-    if request.method == "POST":
-        email = request.form['email']
-        password = request.form['pwd']
-        sql= "select * from USER where email='{}' and password='{}'".format(email,password)
-        stmt = ibm_db.exec_immediate(conn, sql)
-        userdetails = ibm_db.fetch_both(stmt)
-        print(userdetails)
-        if userdetails:
-            session['register'] =userdetails["EMAIL"]
-            return render_template('userprofile.html',name=userdetails["NAME"],email= userdetails["EMAIL"],contact= userdetails["CONTACT"],address=userdetails["ADDRESS"],role=userdetails["ROLE"],branch=userdetails["BRANCH"])
-        else:
-            msg = "Incorrect Email id or Password"
-            return render_template("login.html", msg=msg)
-    return render_template('login.html')
+@app.route("/login",methods=['POST','GET'])
+def loginentered():
 
+      global Userid
+      global Username
+      msg = ''
+      if request.method == "POST" :
+           username = str(request.form['username'])
+           print (username)
+           password = request. form['password']
+
+           sql = "SELECT * FROM users WHERE username=? AND PASSWORD=?"
+
+           stmt = ibm_db.prepare(conn, sql)
+
+           
+
+           ibm_db.bind_param(stmt, 1, username)
+           ibm_db.bind_param(stmt, 2, password)
+           ibm_db. execute(stmt)
+           account = ibm_db.fetch_assoc (stmt)
+           print(account)
+           if account:
+                session['Loggedin'] = True
+                session['userid'] = account['userid']
+                Userid = account['username']
+                session['username'] = account['username']
+                Username = account['USERNAME']
+                Name = account['NAME']
+                msg = "logged in successfully !"
+
+                return render_template('index.html',msg=msg)
+           else:
+                msg='in correct username/password'
+                return render_template('login.html',msg=msg)    
+                #sql ="SELECT ROLE FROM REGISTER where email = ?"
+                #stmt = ibm_db.prepare(conn, sql)
+                #ibm_db.bind_param(stmt, 1, email)
+                #ibm_db.execute (stmt)
+                #r = ibm_db.fetch_assoc(stmt)
+                #print(r)
+                #if r['ROLE'] == 1:
+                 #    print ("STUDENT")
+                  #   return render_template('studentprofile.html', msg=msg, user=email, name=Name,role="STUDENT",username=Username, email=email)
+
+                #elif r['ROLE']== 2:
+                 #      print ("FACULTY")
+                  #     return render_template('Facultyprofile.html', msg=msg, user=email, name=Name,role="FACULTY",username=Username, email=email)
+                #else:
+                 #      return render_template('adminProfile.html', msg=msg, user=email, name=Name,role="ADMIN",username=Username, email=email)
+           #else:
+            #           msg = "Incorrect Email/password"
+
+             #          return render_template('login.html', msg=msg)
+      #else:
+       #         return render_template("login.html")
+
+
+# Rendering Adminprofile.html
+@app.route("/adminprofile")
+def aprofile():
+    return render_template("adminProfile.html")
 
 if __name__ =='__main__':
-    app.run( debug = True)
+    app.run( debug = True,host="0.0.0.0")
                   
--- connect.py
+++ connect.py
@@ -1,39 +1,32 @@
-# -*- coding: utf-8 -*-
 """
-Created on SAT SEP 21:50:15  2023
+Created on  SEP 21:50:15  2023
 
 @author: MYP
 """
 def showall():
-    sql= "SELECT * from USER"
+    sql= "SELECT * from USERS"
     stmt = ibm_db.exec_immediate(conn, sql)
     dictionary = ibm_db.fetch_both(stmt)
     while dictionary != False:
-        print("The Name is : ",  dictionary["NAME"])
-        print("The E-mail is : ", dictionary["EMAIL"])
-        print("The Contact is : ",  dictionary["CONTACT"])
-        print("The Adress is : ",  dictionary["ADDRESS"])
-        print("The Role is : ",  dictionary["ROLE"])
-        print("The Branch is : ",  dictionary["BRANCH"])
-        print("The Password is : ",  dictionary["PASSWORD"])
+        print("The user id is : ",  dictionary["userid"])
+        print("The username is : ", dictionary["username"])
+        print("The email is : ",  dictionary["email"])
+        print("The password is : ",  dictionary["password"])
         dictionary = ibm_db.fetch_both(stmt)
         
 def getdetails(email,password):
-    sql= "select * from USER where email='{}' and password='{}'".format(email,password)
+    sql= "select * from USERS where email='{}' and password='{}'".format(email,password)
     stmt = ibm_db.exec_immediate(conn, sql)
     dictionary = ibm_db.fetch_both(stmt)
     while dictionary != False:
-        print("The Name is : ",  dictionary["NAME"])
+        print("The user id is : ",  dictionary["userid"])
         print("The E-mail is : ", dictionary["EMAIL"])
-        print("The Contact is : ", dictionary["CONTACT"])
-        print("The Address is : ", dictionary["ADDRESS"])
-        print("The Role is : ", dictionary["ROLE"])
-        print("The Branch is : ", dictionary["BRANCH"])
+        print("The user id is : ", dictionary["username"])
         print("The Password is : ", dictionary["PASSWORD"])
         dictionary = ibm_db.fetch_both(stmt)
         
 def insertdb(conn,name,email,contact,address,role,branch,password):
-    sql= "INSERT into USER VALUES('{}','{}','{}','{}','{}','{}','{}')".format(name,email,contact,address,role,branch,password)
+    sql= "INSERT into USERS VALUES('{}','{}','{}','{}')".format(userid,username,email,password)
     stmt = ibm_db.exec_immediate(conn, sql)
     print ("Number of affected rows: ", ibm_db.num_rows(stmt))
 
@@ -42,9 +35,8 @@ try:
     conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=mwv02817;PWD=BGu2G7vYyF7JGCrq",'','')
     print(conn)
     print("connection successful...")
-    #insertdb(conn,"Hari","Hari@gmail.com",'1234567890','Adarsh nagar','Faculty','Civil','1234567')
-    insertdb(conn,"YASEEN","yaseenpasham@svit.ac.in",'9866504099','Vanasthalipuram','Faculty','CSE','123456')
-    getdetails("yaseenpasham@svit.ac.in",'123456')
+    insertdb(conn,"rider2","yaseenpasha",'admin@gmail.com','123456')
+    getdetails("admin@gmail.com",'123456')
     #showall()
 
 except:
